a2e170ed6a8d0bcc0f8a0e0a1a2ce58e423b8527
Chris Johnson <csjohnson79@icloud.com>
2016-09-30 14:12:35 -0600

    First commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Part 1
+
+
+// alert("Hello Boulder!");
+
+// document.write("Hello Boulder!");
+
+
+// Part 2
+
+
+// Use var, prompt, string literals, the + operator, and alert such that when the user opens the page, it will ask them for their name, and then respond with "Hello [YOUR_NAME]!"
+
+var personsName = prompt("What is your name?");
+document.write("Hello " + personsName + "!");
+
+
+
+
+
+
+
+
